b51fc12fe9a53142541c905016fa961a003791b0
Shiva Burade <shvburade@gmail.com>
2022-03-04 12:51:50 +0530

    Update install-maven.sh

--- install-maven.sh
+++ install-maven.sh
@@ -2,6 +2,8 @@ echo "Make sure wget and tar is available\n"
 wget https://repo.maven.apache.org/maven2/org/apache/maven/apache-maven/3.8.1/apache-maven-3.8.1-bin.tar.gz
 tar -xvzf apache-maven-3.8.1-bin.tar.gz
 
+export SOURCE_ROOT=`pwd`
+
 echo "********* export PATH *******************"
 echo "export M2_HOME=$SOURCE_ROOT/apache-maven-3.8.1"
 echo "export PATH=$M2_HOME/bin:$PATH"
