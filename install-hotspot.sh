27e67c708795a56598497877fda6d6b15f55c4fe
Shiva Burade <shvburade@gmail.com>
2022-03-04 12:35:51 +0530

    Update install-hotspot.sh

--- install-hotspot.sh
+++ install-hotspot.sh
@@ -1,4 +1,6 @@
 export SOURCE_ROOT=`pwd`
 wget https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.13%2B8/OpenJDK11U-jdk_s390x_linux_hotspot_11.0.13_8.tar.gz
 tar -xf OpenJDK11U-jdk_s390x_linux_hotspot_11.0.13_8.tar.gz
+echo "***** run below commands to set env *********"
 echo "export JAVA_HOME=$SOURCE_ROOT/jdk-11.0.13+8"
+echo "export PATH=$JAVA_HOME/bin:$PATH"
