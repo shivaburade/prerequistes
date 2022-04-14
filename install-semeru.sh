a1527719358f3433ec9c54e621b6ec0110d7139c
Shiva Burade <shvburade@gmail.com>
2022-04-14 15:24:13 +0530

    Create install-semeru.sh

---
+++ install-semeru.sh
@@ -0,0 +1,2 @@
+curl -SL -o semeru.tar.gz https://github.com/ibmruntimes/semeru8-binaries/releases/download/jdk8u322-b06_openj9-0.30.0/ibm-semeru-open-jdk_s390x_linux_8u322b06_openj9-0.30.0.tar.gz
+tar -xvf semeru.tar.gz
